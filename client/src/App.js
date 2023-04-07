b6b7894bd76f20d4c251b1801e02ca69e96844b7
nomanaijaz <noman.aijaz@startrum.com>
2023-04-07 12:49:11 +0500

    create a basic templatee for frontend

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <>App frontend is running</>;
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors'
-import dotenv from 'dotenv';
-import path from 'path';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import { fileURLToPath } from 'url';
-import connection  from  './database/connection.js';
-import authRoutes  from './routes/auth.js';
-import userRoutes from './routes/users.js'
-import userPosts from './routes/posts.js'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import helmet from "helmet";
+import morgan from "morgan";
+import { fileURLToPath } from "url";
+import connection from "./database/connection.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import userPosts from "./routes/posts.js";
 
 //  CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
@@ -17,24 +17,26 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({limit:'30mb', extended:true}));
-app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 // connected with database
 connection();
 
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/posts", userPosts);
 
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));  
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
 
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
-app.use('/posts', userPosts)
+const PORT = process.env.PORT | 9000;
 
-const  PORT  =  process.env.PORT | 9000;
-
-app.listen(PORT, ()=>console.log(`Listening PORT = ${PORT}`));
+app.listen(PORT, () => console.log(`Listening PORT = ${PORT}`));

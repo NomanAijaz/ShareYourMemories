c261f694e423476059311cf7bd5c7717594fd659
nomanaijaz <noman.aijaz@startrum.com>
2023-04-16 18:09:53 +0500

    created post successfully

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -4,18 +4,21 @@ import User from "../models/user.js";
 export const createPost = async (req, res) => {
   try {
     const { userId, description, picturePath } = req.body;
-    const user = await User.findOne(userId).select({
-      firstName: 1,
-      lastName: 1,
-      location: 1,
-      picturePath: 1,
-    });
+    const user = await User.findOne({ _id: userId })
+      .select({
+        firstName: 1,
+        lastName: 1,
+        location: 1,
+        picturePath: 1,
+      })
+      .lean();
+    const { firstName, lastName, location } = user;
 
     const newPost = new Post({
       userId,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      location: user.location,
+      firstName,
+      lastName,
+      location,
       description,
       userPicturePath: user.picturePath,
       picturePath,
@@ -28,7 +31,7 @@ export const createPost = async (req, res) => {
     const post = await Post.find();
     res
       .status(200)
-      .send({ message: "Got Post successfully", status: true, data: post });
+      .send({ message: "Post successfully", status: true, data: post });
   } catch (error) {
     res.status(401).send({ error: error.message });
   }
@@ -46,11 +49,13 @@ export const getFeedPosts = async (req, res) => {
 
 export const getUserPosts = async (req, res) => {
   try {
-    const {userId}  = req.params;
-    const post  = await Post.find({userId})
-    res
-    .status(200)
-    .send({ message: "Got user Posts successfully", status: true, data: post });
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).send({
+      message: "Got user Posts successfully",
+      status: true,
+      data: post,
+    });
   } catch (error) {
     res.status(400).send(error.message);
   }
@@ -58,21 +63,26 @@ export const getUserPosts = async (req, res) => {
 
 export const likePost = async (req, res) => {
   try {
-
-    const {id} = req.params;
-    const {userId}  = req.body;
-    const post  = await Post.find({id})
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.find({ id });
     const isLiked = post.likes.get(userId);
 
-    if(isLiked){
-        post.likes.delete(userId)
-    }else{
-        post.likes.set(userId, true)
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
     }
-    const updatedPost = await Post.findOneAndUpdate(id, {likes:post.likes},{new: true})
-    res
-    .status(200)
-    .send({ message: "Got Updated Post successfully", status: true, data:updatedPost });
+    const updatedPost = await Post.findOneAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+    res.status(200).send({
+      message: "Got Updated Post successfully",
+      status: true,
+      data: updatedPost,
+    });
   } catch (error) {
     res.status(400).send(error.message);
   }
